99b911741371353d815910a0b399ae6f57a57f5b
ciovnicudenis <dciovnicu@gmail.com>
2026-07-14 18:05:50 +0300

    added lightning, mouse controls & my 3d logo model

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'https://unpkg.com/three@latest/examples/jsm/loaders/DRACOLoader.js';
 
 const scene = new THREE.Scene();
 const canvas = document.getElementById('experience-canvas');
@@ -6,38 +9,76 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
 };
-const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
 
 // creating the renderer
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize( sizes.width, sizes.height );
-document.body.appendChild( renderer.domElement );
+renderer.setPixelRatio( Math.min(window.devicePixelRatio, 2) );
+
+// initializing DRACOLoader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/'); 
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader); // link it to the GLTFLoader
+
+// loading the 3d model
+loader.load( 
+    './assets/3dlogo.glb', 
+    function ( glb ) { 
+        //console.log(glb.scene.children[0].name);
+        scene.add( glb.scene ); 
+    }, 
+    undefined, 
+    function ( error ) { console.error( error ); } 
+);
+
+// adding light
+// directional light
+const color = 0xFFFFFF;
+const intensity = 3;
+const directionalLight = new THREE.DirectionalLight(color, intensity);
+directionalLight.position.set(0, 10, 3);
+directionalLight.target.position.set(-2, 0, 0);
+scene.add(directionalLight);
+scene.add(directionalLight.target);
+
+// hemisphere light
+const skyColor = 0xB1E1FF;  // light blue
+const groundColor = 38800420;  // dark green
+const hemisphereLight = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+scene.add(hemisphereLight);
+
+// creating the camera
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 ); 
+
+// creating the arcball controls for the camera
+const controls = new OrbitControls( camera, canvas );
+controls.enableDamping = true; // Adds buttery smooth physics
+controls.dampingFactor = 0.05;
+controls.update(); // updating the controls 
 
 // i got the geometry like all the dots forming my mesh and materials, etc forming
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5; // moving the camera away from the cube so we can see it
+camera.position.z = 1; // moving the camera away from the cube so we can see it
 
 function handleResize() { // calling this function whenever the window is resized
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
     camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
-    
-    console.log("resizing");
-
+    //console.log("resizing");
     renderer.setSize( sizes.width, sizes.height );
+renderer.setPixelRatio( Math.min(window.devicePixelRatio, 2) );
 }
 
 window.addEventListener( 'resize', handleResize );
 
 function animate( time ) { // little animation for our cube :)
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
+    //console.log("animating");
+    controls.update();
     renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
